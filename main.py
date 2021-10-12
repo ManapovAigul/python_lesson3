860f0353c81caeb8441915700088188ee1b0679e
Manapov <brave-girl.90@mail.ru>
2021-10-12 20:50:36 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+dateOfBirthday = 1994 # camel case
+date_of_birthday = 1993 # Snake case
+var_int = 1  # variable integer(число)
+var_float = 1.2  # 
+var_str = "Murat"  # Variable string(строка)
+var_boolean_true = True  # Variable boolean(истина)
+var_boolean_false = False  # Variable boolean(лож)
+var_list = [1, 4, 5,  "Murat", [1, 4, 5,], (1, 4, 5), {"name": "Murat"}]  # Variable list(список)
+var_tuple = (1, [1, 2],  "Murat",)  # Variable tuple(кортеж)
+var_dict = {"name":"Айгуль"}
